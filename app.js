85c13c70c31132c2bf339d76fd0d7c3839e18459
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-07 01:45:24 -0800

    Modified NYTExercise.html and app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 
-$("#runSearch").on("click", function () {
-    var queryString = $("#searchTerm").val().trim();
-    var beginDate = $("#startYear").val().trim();
-    var endDate = $("#endYear").val().trim();
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-        'api-key': "3b42f0f619774ed6abb205159eeafd5e",
-        'q': queryString,
-        'begin_date': beginDate,
-        'end_date': endDate
-    });
+    var queryString = "economy";
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + queryString + "&api-key=3b42f0f619774ed6abb205159eeafd5e";
 
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-        });
-
-});
+      url: url,
+      method: 'GET'
+    }).done(function(result) {
+      console.log(result);
+      var newArticleDiv = $("<div>");
+      var data = result.response.docs;
+      for (var i=0; i< data.length; i++){
+          newArticleDiv.append("<strong>" + data[i].snippet + "</strong><br>");
+          newArticleDiv.append("<em>" + data[i].web_url + "</em><br><br>");
+      }
+      $("#wellSection").prepend(newArticleDiv);
+    });
