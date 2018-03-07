8054d4a024ac2c268a68e6485175da96f1a06f5d
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-07 12:47:16 -0800

    Fixed event handlers for search and clear buttons

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-
-    var queryString = "economy";
+$("#runSearch").on("click", function (event) {
+    event.preventDefault();
+    var queryString = $("#searchTerm").val().trim();
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + queryString + "&api-key=3b42f0f619774ed6abb205159eeafd5e";
 
     $.ajax({
-      url: url,
-      method: 'GET'
-    }).done(function(result) {
-      console.log(result);
-      var newArticleDiv = $("<div>");
-      var data = result.response.docs;
-      for (var i=0; i< data.length; i++){
-          newArticleDiv.append("<strong>" + data[i].snippet + "</strong><br>");
-          newArticleDiv.append("<em>" + data[i].web_url + "</em><br><br>");
-      }
-      $("#wellSection").prepend(newArticleDiv);
+        url: url,
+        method: 'GET'
+    }).done(function (result) {
+        console.log(result);
+        var newArticleDiv = $("<div>");
+        var data = result.response.docs;
+        for (var i = 0; i < data.length; i++) {
+            newArticleDiv.append("<strong>" + data[i].snippet + "</strong><br>");
+            newArticleDiv.append("<em>" + data[i].web_url + "</em><br><br>");
+        }
+        $("#wellSection").append(newArticleDiv);
     });
+
+});
+
+$("#clearAll").on("click", function () {
+    $("#wellSection").empty();
+});
